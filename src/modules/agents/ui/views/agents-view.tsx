50c40165c997fd284651b2794c339f376138e2a0
amoorawat <ar.amanrawat1009@gmail.com>
2026-03-18 12:47:31 +0530

    agent setup

---
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { LoadingState } from "@/components/loading-state"
+import { useTRPC } from "@/trpc/client"
+import {  useSuspenseQuery } from "@tanstack/react-query"
+
+export const AgentsView = () => {
+    const trpc = useTRPC()
+    const { data, } = useSuspenseQuery(trpc.agents.getMany.queryOptions())
+
+    return(
+        <div>
+            {JSON.stringify(data, null, 2)}
+        </div>
+    )
+}
